6a2ce49bce767a869bbdbc87d9802130ab3b3f88
Daniel <itzdanielmichael@gmail.com>
2021-04-19 11:32:28 -0400

    create login.jsx

--- src/utils/globalUtils.js
+++ src/utils/globalUtils.js
@@ -1,4 +1,14 @@
 export const COLORS = {
   DARK_BLUE: '#040714',
+  OCEAN_BLUE: '#0063e5',
+  SKY_BLUE: '#0483ee',
   DISNEY_WHITE: '#f9f9f9',
+};
+
+export const IMAGES = {
+  WELCOME_BG: '/images/login-background.jpg',
+  WELCOME_LOGOS_1:
+    'https://cannonball-cdn.bamgrid.com/assets/originals/bundle-logo-01.svg',
+  WELCOME_LOGOS_2:
+    'https://cannonball-cdn.bamgrid.com/assets/originals/disney-franchise-logos.png',
 };
